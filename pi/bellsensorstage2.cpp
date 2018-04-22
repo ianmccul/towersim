a5726c07e0c337054215615f50112d45b2a1d92f
Ian McCulloch <ianmcc@physics.uq.edu.au>
2018-04-22 19:53:15 +1000

    typo in previous commit

--- pi/bellsensorstage2.cpp
+++ pi/bellsensorstage2.cpp
@@ -635,7 +635,7 @@ int main(int argc, char** argv)
             int NumAccel = (Flags & 0x70) >> 4;
             int NumGyro = Flags & 0x0F;
 
-            int ExpectedPacketLength = NumAccel*4 + NumGyro*2 + 4 + 1 + 8 + 4  2;
+            int ExpectedPacketLength = NumAccel*4 + NumGyro*2 + 4 + 1 + 8 + 4 + 2;
 
             if (len != ExpectedPacketLength)
             {
