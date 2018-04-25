b4cf7a21602e201fa4636da438038de1b68dd7e5
Ian McCulloch <ianmcc@physics.uq.edu.au>
2018-04-25 22:57:23 +1000

    accel

--- pi/calibrate-accel.cpp
+++ pi/calibrate-accel.cpp
@@ -160,7 +160,7 @@ void SolveCalibrationParameters()
       A(i*2+1, 4) = 0.0;
       A(i*2+1, 5) = 1.0;
 
-      b[i*2] = std::cos(i*pi/4.0);
+      b[i*2+1] = std::cos(i*pi/4.0);
    }
 
    Eigen::VectorXf x(6);
