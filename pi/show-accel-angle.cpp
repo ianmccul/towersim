03760b9fec3bcc5aadf4d5348af850b3710f3b12
Ian McCulloch <ianmcc@physics.uq.edu.au>
2018-04-26 02:10:53 +1000

    accel

--- pi/show-accel-angle.cpp
+++ pi/show-accel-angle.cpp
@@ -14,8 +14,8 @@ constexpr double g = 9.80665;
 std::tuple<double, double>
 Correction(double ax, double ay)
 {
-   double rax =  1.026667e-1*ax + 1.951888e-4*ay + 2.9487651e-3;
-   double ray = -4.200339e-5*ax + 1.025088e-1*ay - 2.2330396e-2;
+   double rax = -1.026667e-1*ax + -1.951888e-4*ay + -2.9487651e-3;
+   double ray =  4.200339e-5*ax + -1.025088e-1*ay +  2.2330396e-2;
 
    //   double rax = -1.026667e-1*ax - 1.951888e-4*ay - 2.9487651e-3;
    //   double ray =  4.200339e-5*ax - 1.025088e-1*ay + 2.2330396e-2;
@@ -33,7 +33,7 @@ int main(int argc, char** argv)
    double last_gyro = -1000;
    while (std::cin >> Time >> ax >> ay)
    {
-      std::tie(ax, ay) = Correction(-ax, -ay);
-      std::cout << Time << ' ' << std::atan2(ax,ay) * 180.0 / M_PI << std::endl;
+      std::tie(ax, ay) = Correction(ax, ay);
+      std::cout << Time << ' ' << std::atan2(-ax,-ay) * 180.0 / M_PI << std::endl;
    }
 }
